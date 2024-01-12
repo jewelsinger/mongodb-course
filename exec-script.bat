5a63cdded87ea996b06fac7f356d3c9eff01641a
memol <memola.bruno@gmail.com>
2024-01-12 12:06:53 +0100

    exec-script

---
+++ exec-script.bat
@@ -0,0 +1,1 @@
+docker exec -it mongo-db /bin/bash -c "mongosh --nodb --file script.js"
--- workspace/script.js
+++ workspace/script.js
@@ -1,2 +1,5 @@
 let db = connect("mongodb://root:test123@localhost");
 
+//liste les bases de données
+let dbList = db.adminCommand("listDatabases");
+console.log(dbList);
